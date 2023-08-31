a17d9a0086717a125db1b4cbfc630faff6ccc24d
mugane-dj <muganedev25@gmail.com>
2023-08-31 12:21:48 +0300

    0x0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    The function `filter_datum` obfuscates a list of log messages
+    
+    :param fields: a list of strings representing all fields to obfuscate
+    :param redaction: a string representing by what the field will be obfuscated
+    :param message: a string representing the log line
+    :param separator: a string representing by which character is separating all
+                      fields in the log line (message)
+    :return: the log message obfuscated
+    """
+    for field in fields:
+        pattern = r'{}=.*?{}'.format(field, separator)
+        replacement = "{}={}{}".format(field, redaction, separator)
+        message = re.sub(pattern, replacement, message)
+    return message
