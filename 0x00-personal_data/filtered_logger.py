ea867389ea8ce20b6c60d5557048f52b18875f91
mugane-dj <muganedev25@gmail.com>
2023-08-31 12:35:13 +0300

    0x0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,10 +13,11 @@ def filter_datum(
     The function `filter_datum` obfuscates a list of log messages
 
     :param fields: a list of strings representing all fields to obfuscate
-    :param redaction: a string representing by what the field will be obfuscated
+    :param redaction: a string representing by what the field will be
+                      obfuscated
     :param message: a string representing the log line
-    :param separator: a string representing by which character is separating all
-                      fields in the log line (message)
+    :param separator: a string representing by which character is separating
+                      all fields in the log line (message)
     :return: the log message obfuscated
     """
     for field in fields:
