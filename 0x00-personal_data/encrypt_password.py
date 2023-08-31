6c4552a6bc52e34869913ecbb556a00574c7fe24
mugane-dj <muganedev25@gmail.com>
2023-08-31 15:59:50 +0300

    0x6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,11 +5,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Hashes a password"""
-    return bcrypt.hashpw(password)
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks if password matches the hashed_password provided"""
-    if bcrypt.hashpw(password) == hashed_password:
-        return True
-    return False
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
