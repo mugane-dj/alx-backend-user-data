38f1dd2881270cba1c6045c530b60932fe710b33
mugane-dj <muganedev25@gmail.com>
2023-09-05 10:46:49 +0300

    0x0

--- 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/ec2f874b061bd3a2915949f081f4f5f055104f20/SimpleAPI/api/v1/app.py
@@ -4,8 +4,9 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
 
 
 app = Flask(__name__)
@@ -15,7 +16,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
