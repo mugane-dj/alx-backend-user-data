b9be76a3f703dbfb8cc5b9a990297427716a08da
mugane-dj <muganedev25@gmail.com>
2023-09-05 12:13:52 +0300

    0x2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,3 +35,13 @@ def unauthorized() -> str:
       - 401 Error
     """
     return abort(401)
+
+
+@app_views.route("/forbidden/", strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+      - 403 Error
+    """
+    return abort(403)
