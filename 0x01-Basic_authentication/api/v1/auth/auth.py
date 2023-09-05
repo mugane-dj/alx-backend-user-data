ed094112db7b4e666890a9bfab961395b3576de0
mugane-dj <muganedev25@gmail.com>
2023-09-05 15:19:19 +0300

    0x5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Current user"""
