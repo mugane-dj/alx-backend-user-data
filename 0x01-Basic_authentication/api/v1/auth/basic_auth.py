e4be67822c6b5edec3315149286d9daf977949fc
mugane-dj <muganedev25@gmail.com>
2023-09-05 16:49:25 +0300

    0xA

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 An implementation of basic authentication
 """
 import base64
+from typing import TypeVar
+from models.user import User
 from api.v1.auth.auth import Auth
 
 
@@ -59,3 +61,28 @@ class BasicAuth(Auth):
         email = decoded_base64_authorization_header.split(":")[0]
         password = decoded_base64_authorization_header.split(":")[-1]
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar("User"):
+        """
+        Create User instance based on user email and password
+        """
+        if (
+            user_email is None
+            or user_pwd is None
+            or not isinstance(user_email, str)
+            or not isinstance(user_pwd, str)
+        ):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
