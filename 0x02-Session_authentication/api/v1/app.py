5830d105dbc6a0f799f433515ff93d39331b25f9
mugane-dj <muganedev25@gmail.com>
2023-09-07 21:46:08 +0300

    0x5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,13 +34,18 @@ def handle_before_request():
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
     ]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        return abort(401)
+    if (
+        auth.authorization_header(request) is None
+        and auth.session_cookie(request) is None
+    ):
+        abort(401)
+        return None
 
     if auth.current_user(request) is None:
         return abort(403)
