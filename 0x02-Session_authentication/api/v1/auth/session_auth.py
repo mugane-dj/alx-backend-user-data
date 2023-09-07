e5ec7b544311e99215d4d1a94fbc841636ab742a
mugane-dj <muganedev25@gmail.com>
2023-09-07 22:44:25 +0300

    0x6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,13 +21,13 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Retrieves user based on session_id provided"""
-        if session_id is None or not isinstance(session_id, uuid.UUID):
+        """Retrieves user_id based on session_id provided"""
+        if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """Retrieves current User instance based on a cookie value"""
         session_id = super().session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id=session_id)
+        user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
