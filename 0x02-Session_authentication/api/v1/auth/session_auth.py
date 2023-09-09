6ea38660e45f172b33db4d5398ed67e253139fde
mugane-dj <muganedev25@gmail.com>
2023-09-10 00:22:03 +0300

    0x6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,20 +17,20 @@ class SessionAuth(Auth):
         """Creates a Session ID for a user"""
         if user_id is None or not isinstance(user_id, str):
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Retrieves user_id based on session_id provided"""
-        if session_id is None or not isinstance(session_id, uuid.UUID):
+        if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Retrieves current User instance based on a cookie value"""
         session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(uuid.UUID(session_id))
+        user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
 
     def destroy_session(self, request=None) -> bool:
