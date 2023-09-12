1c99918a930203fca731e227f0663bae2d849521
mugane-dj <muganedev25@gmail.com>
2023-09-12 12:24:37 +0300

    0x1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,10 +32,9 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
         """Add user to DB"""
-        session = self._session
         user = User(email=email, hashed_password=hashed_password)
-        session.add(user)
-        session.commit()
+        self._session.add(user)
+        self._session.commit()
         return user
 
     # def find_user_by(self, **kwargs) -> TypeVar("User"):
