a623593ab3169d3cfef4f0df0c721f23212d256d
mugane-dj <muganedev25@gmail.com>
2023-09-12 12:32:24 +0300

    0x1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,11 +4,8 @@ DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from typing import TypeVar
 from user import Base, User
 
 
@@ -32,29 +29,25 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add user to DB"""
-        try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            user = None
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
         return user
 
-    # def find_user_by(self, **kwargs) -> User:
-    #     """Find user in DB"""
-    #     session = self._session
-    #     user = session.query(User).filter_by(**kwargs).first()
-    #     if not user:
-    #         raise NoResultFound
-    #     return user
+    def find_user_by(self, **kwargs) -> User:
+        """Find user in DB"""
+        session = self._session
+        user = session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
 
-    # def update_user(self, user_id: int, **kwargs) -> None:
-    #     """Update a user instance"""
-    #     user = self.find_user_by(id=user_id)
-    #     session = self._session
-    #     for k, v in kwargs.items():
-    #         if k not in dir(User):
-    #             raise ValueError
-    #         setattr(user, k, v)
-    #     session.commit()
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user instance"""
+        user = self.find_user_by(id=user_id)
+        session = self._session
+        for k, v in kwargs.items():
+            if k not in dir(User):
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
