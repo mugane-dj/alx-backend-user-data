04336df49a75cbfbdbf7ce9e0c21b33f220f26b3
mugane-dj <muganedev25@gmail.com>
2023-09-12 12:29:33 +0300

    0x1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add user to DB"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
