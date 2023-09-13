ff80c527cd07aa4b908d9f89fcd92f348e7bcd59
mugane-dj <muganedev25@gmail.com>
2023-09-13 20:17:24 +0300

    0xD

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,23 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Returns user based on session_id provided"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: str) -> None:
+        """Updates the corresponding user’s session ID to None"""
+        if user_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(user_id=user_id)
+            user.session_id = None
+        except NoResultFound:
+            return None
