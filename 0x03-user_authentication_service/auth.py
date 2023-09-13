8caabd4cf16c275f5dd78239b912358dd61d47c9
mugane-dj <muganedev25@gmail.com>
2023-09-13 20:42:09 +0300

    0x10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,3 +72,13 @@ class Auth:
             user.session_id = None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generate reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            user.reset_token = reset_token
+            return reset_token
+        except NoResultFound:
+            raise ValueError
