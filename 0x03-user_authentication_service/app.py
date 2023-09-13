5bd05bcbf88abf4a20802c9acdd369e6449daf1a
mugane-dj <muganedev25@gmail.com>
2023-09-13 20:36:44 +0300

    0xF

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index():
     """Home route"""
     return jsonify({"message": "Bienvenue"})
@@ -49,6 +49,16 @@ def logout():
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """Retrieve user profile"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id=session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
